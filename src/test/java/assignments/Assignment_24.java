5611177df532a15b7b12ed755fe32cf42ca8c3c7
anu <anugiit1992@gmail.com>
2025-06-12 12:30:32 +0530

    Assignment_24

--- src/test/java/assignments/Assignment_24.java
+++ src/test/java/assignments/Assignment_24.java
@@ -1,3 +1,12 @@
+package assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment_24 {
+
 /*
 Assignment - 24
 =============
@@ -15,3 +24,30 @@ Assignment - 24
        
 //7.Close browser window
 */
+
+	public static void main(String[] args) {
+		
+		//1.Launch Chrome Browser
+		WebDriver driver = new ChromeDriver();
+		
+		//2.Maximize the browser window
+		driver.manage().window().maximize();
+		
+		//3.Launch the application (https://demoqa.com/frames)
+		driver.get("https://demoqa.com/frames");
+		
+		//4.Locate elements
+		WebElement frameElement = driver.findElement(By.xpath("//iframe[@id='frame1']"));
+		WebElement mainWindowElement = driver.findElement(By.id("sampleHeading"));
+		
+		//5.Print frame element text
+		System.out.println("Frame Element Text: " + frameElement.getText());
+		
+		//6.Print main window element text
+		System.out.println("Main Window Element Text: " + mainWindowElement.getText());
+		
+		//7.Close browser window
+		driver.quit();
+	}
+	
+}
